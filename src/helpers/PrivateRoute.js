b8c6e821d6328e738e71f5e7456e76e5faa5ab8a
Dani Crosby <dani.crosby1111@gmail.com>
2021-05-26 10:20:43 -0500

    added private routes file

---
+++ src/helpers/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  const routeChecker = (taco) => (user
+    ? (<Component {...taco} user={user} />)
+    : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any
+};
+
+export default PrivateRoute;
