de541b8e912a6d98c410d524b5d3669587002785
Dani Crosby <dani.crosby1111@gmail.com>
2021-05-30 19:05:14 -0500

    packages working on deploy, fixed spacing bug from package aftermath

--- src/helpers/views/Home.js
+++ src/helpers/views/Home.js
@@ -33,7 +33,7 @@ export default function Home() {
         </div>
       </nav>
 
-      <div className="col-lg-3 col-md-3 col-sm-2 image-container">
+      <div className="col-lg-4 col-md-3 col-sm-2 image-container">
         <img className="profile-image" src={profile} />
       </div>
 
